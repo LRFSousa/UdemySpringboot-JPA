d5563b4ac5859a28d55b221f316935d75051718b
LRFSousa <luisricardofs@gmail.com>
2023-10-14 18:16:05 -0300

    first commit

---
+++ src/main/java/com/LuisRicardo/CursoUdemy/CursoUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.LuisRicardo.CursoUdemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUdemyApplication.class, args);
+	}
+
+}
